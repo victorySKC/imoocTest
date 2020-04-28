60f223b85366ea8692dfb6b82c0bbd007ad025ca
victorySKC <2289799316@qq.com>
2020-04-28 10:46:43 +0800

    买家端Controller开发。是控制前端页面交互的开发层

---
+++ sell/src/main/java/com/kd/sell/controller/BuyerOrderController.java
@@ -0,0 +1,24 @@
+package com.kd.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: sell
+ * @description: 买家端客户端controller层
+ * @author: sikuncheng
+ * @create: 2020-04-27 14:27
+ **/
+@RestController
+@RequestMapping("/buyer/order")
+@Slf4j
+public class BuyerOrderController {
+    //创建订单
+
+    //订单列表
+
+    //订单详情
+
+    //取消订单
+}
